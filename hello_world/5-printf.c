03ecaf5d160f33adcc712e12ad2b4b2364d4056a
Marivellisse <gmarivellisse@gmail.com>
2024-10-21 20:36:44 -0400

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - Prints "with proper grammar, but the outcome is a piece of art".
 * Return: Return 0 when done.
 */
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+       printf("with proper grammar, but the outcome is a piece of art,\n");
+       return (0);
 }
