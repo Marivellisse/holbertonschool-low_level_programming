e4c24fb5c2d3f4d195aad93aa750cea65b7f07c0
Marivellisse <gmarivellisse@gmail.com>
2025-02-13 11:53:49 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Return 0 whn done.
+ * Return: Return 0 when done.
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	retun (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
