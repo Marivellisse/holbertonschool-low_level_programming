7035eb2934b0265303596a296c5fd6b8a687efe2
Marivellisse <gmarivellisse@gmail.com>
2025-02-13 11:41:10 -0400

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Return 0 whn done.
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	retun (0);
+}
