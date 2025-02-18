ab3bc054d5d7cced34300279d8655255ddd92fa3
Marivellisse <gmarivellisse@gmail.com>
2025-02-18 13:47:50 -0400

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- *  main - Entry main printing _putchar in single line
- *  Return: Always 0 (Success)
- */
-
+ *  main - Print var
+ *
+ *  Return: Always 0
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+char *p = _"putchar";
 
+	while (*p)
+	{
+		_putchar(*p);
+			p++;
+	}
+
+_putchar('\n');
 	return (0);
-}
+		}
