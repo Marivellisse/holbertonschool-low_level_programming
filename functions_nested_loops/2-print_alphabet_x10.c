c926eeb673bda6ab7da9d23d04e61627c35145d2
Marivellisse <gmarivellisse@gmail.com>
2025-02-19 11:08:24 -0400

    2print

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,19 @@
 /**
  * print_alphabet_x10 - print the alphabet x10
  *
- *
- void print_alphabet_x10(void)
- {
- int abc, cou;
-
- * This count the amount of time. The alphabet is printed*/
-cou = 0;
-while (cou < 10)
+ */
+void print_alphabet_x10(void)
 {
-	/* This print the alphabet with a newline*/
-	for (abc = 'a'; abc <= 'z'; abc++)
-		_putchar(abc);
-	_putchar('\n');
-	cou++;
-}
-}
+	int abc, cou;
+
+	/* This count the amount of time. The alphabet is printed*/
+	cou = 0;
+	while (cou < 10)
+	{
+		/* This print the alphabet with a newline*/
+		for (abc = 'a'; abc <= 'z'; abc++)
+			_putchar(abc);
+		_putchar('\n');
+		cou++;
+	}
+	u}
