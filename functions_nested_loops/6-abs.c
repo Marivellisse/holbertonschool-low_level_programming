717629b354ecbe9dd3a8bc1cddc5208f535d8308
Marivellisse <gmarivellisse@gmail.com>
2025-02-20 18:38:23 -0400

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,17 +1,29 @@
 #include "main.h"
+#include "main.h"
+
 /**
- * _abs - give the asbolute value
- * @n: It's the number
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer whose absolute value is computed.
+ *
+ * Return: The absolute value of the integer.
  */
-int abs(int n)
+int _abs(int n)
 {
-	/* This is work out the negative signs to make it the asbolute value*/
 	if (n < 0)
-	{
-		n = (-1) * n;
-		return (n);
-	}
-	else if (n > 0)
-		return (n);
-	return (0);
+		return (-n);  /* If n is negative, return its positive counterpart */
+	else
+		return (n);   /* If n is positive or zero, return it as is */
+}
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer whose absolute value is computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);  /* If n is negative, return its positive counterpart */
+	else
+		return (n);   /* If n is positive or zero, return it as is */
 }
