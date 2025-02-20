087bf43d71987faaf3c352321f144c59bf66786a
Marivellisse <gmarivellisse@gmail.com>
2025-02-20 10:40:00 -0400

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,22 +1,17 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _abs - give the asbolute value
+ * @n: It's the number
  */
-int main(void)
+int _abs(int n)
 {
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+	/* This is work out the negative signs to make it the asbolute value*/
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+	else if (n > 0)
+		return (n);
+	return (0);
 }
