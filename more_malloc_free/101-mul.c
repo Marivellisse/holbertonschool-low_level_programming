2a70c49b78b441200b38918326e30bb58455307a
Marivellisse <gmarivellisse@gmail.com>
2025-03-10 17:17:35 -0400

    101

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,40 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <ctype.h>
+#include <string.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- *
+ * is_number - checks if a string is composed of only digits
+ * @s: string to check
+ * Return: 1 if only digits, 0 otherwise
+ */
+int is_number(char *s)
+{
+	while (*s)
+	{
+		if (!isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+/**
+ * multiply - Multiplies two numbers represented as strings
+ * @num1: First number string
+ * @num2: Second number string
+ * Return: Result as a dynamically allocated string
+ */
+char *multiply(char *num1, char *num2)
+{
+	int len1 = strlen(num1), len2 = strlen(num2);
+	int len = len1 + len2;
+	int *result = calloc(len, sizeof(int));
+	char *res_str;
+	int i, j, carry, n1, n2, sum;
+
+	if (!result)
+		return (NULL);
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + result[i + j + 1] + carry;
+			carry = sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+		result[i + j + 1] += carry;
+	}
+
+	while (*result == 0 && len > 1)
+	{
+		result++;
+		len--;
+	}
+
+	res_str = malloc(len + 1);
+	if (!res_str)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		res_str[i] = result[i] + '0';
+	res_str[len] = '\0';
+
+	return (res_str);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
  * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int len1, len2, len;
-	int *result;
+	char *result;
 
-	if (argc != 3)
-		print_error();
+	if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-	num1 = argv[1];
-	num2 = argv[2];
+	result = multiply(argv[1], argv[2]);
+	if (!result)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-	if (!_isdigit(num1) || !_isdigit(num2))
-		print_error();
-
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len = len1 + len2 + 1;
-
-	result = init_result_array(len);
-
-	multiply(num1, num2, result, len1, len2);
-
-	print_result(result, len);
-
+	printf("%s\n", result);
 	free(result);
+
 	return (0);
 }
 
