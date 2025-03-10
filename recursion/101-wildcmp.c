d8aba948352b9a06fefec493fb4d870b6d61a69f
Marivellisse <gmarivellisse@gmail.com>
2025-03-09 20:03:43 -0400

    101

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if they can be considered identical
- * @s1: the first string
- * @s2: the second string (may contain wildcard '*')
+ * wildcmp - compares two strings and returns 1 if they can be considered
+ * identical
+ * @s1: first string
+ * @s2: second string (may contain '*')
  *
  * Return: 1 if identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	/* If both strings reach the end, they match */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	/* If s2 has '*', it can match zero or more characters in s1 */
 	if (*s2 == '*')
 	{
-		/* Move to the next character in s2 OR */
-		/* stay at current * in s2 but move in s1 */
-		return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
 	}
 
-	/* If current characters match (including when both are '\0') */
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
-	/* No match */
 	return (0);
 }
+
